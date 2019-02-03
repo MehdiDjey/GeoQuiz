be1e93c9559af2f12d01919aeec32e0e3c7a027a
Mehdi Djermoune <mehdi.djermoune11@gmail.com>
2019-02-03 16:03:33 +0100

    first manipulation of geocoder and get location randomly in the map by using name

--- app/src/main/java/com/example/geoquiz/MapQuestion.java
+++ app/src/main/java/com/example/geoquiz/MapQuestion.java
@@ -1,31 +1,74 @@
 package com.example.geoquiz;
 
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
 
 public class MapQuestion extends AppCompatActivity implements OnMapReadyCallback {
-    private MapView mapView;
-    private GoogleMap gmap;
+     MapView mapView;
+     GoogleMap gmap;
+     TextView question_situer;
+      TextView distance;
+      TextView randomCountry;
+
+
+
     private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
+    private DataBaseHelper dbHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_question);
-
+        findByView();
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
         }
-
-        mapView = findViewById(R.id.mapView);
         mapView.onCreate(mapViewBundle);
         mapView.getMapAsync(this);
+        dbHelper = new DataBaseHelper(this);
+        randomCountry.setText(randomCapital());
+
+
+    }
+
+    String randomCountry() {
+            Random rnd = new Random();
+           return dbHelper.getAll().get(rnd.nextInt(dbHelper.getAll().size())).getPays();
+    }
+
+    String randomCapital(){
+        Random rnd = new Random();
+         return dbHelper.getAll().get(rnd.nextInt(dbHelper.getAll().size())).getCapitale();
+    }
+
+    public void findByView() {
+        mapView = findViewById(R.id.mapView);
+        question_situer= findViewById(R.id.tv_situer);
+        distance = findViewById(R.id.tv_distance);
+        randomCountry = findViewById(R.id.country_tv);
+
     }
 
     @Override
@@ -76,7 +119,56 @@ public class MapQuestion extends AppCompatActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         gmap = googleMap;
         gmap.setMinZoomPreference(12);
+        gmap.setIndoorEnabled(true);
+
+        UiSettings uiSettings = gmap.getUiSettings();
+        uiSettings.setIndoorLevelPickerEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setZoomControlsEnabled(true);
+
         LatLng ny = new LatLng(40.7143528, -74.0059731);
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(ny);
+        gmap.addMarker(markerOptions);
+
         gmap.moveCamera(CameraUpdateFactory.newLatLng(ny));
+
+        goToLocationByName();
+
+    }
+
+    public  void goToLocationByName() {
+        List<Address> addressList = null;
+        String location = randomCountry.getText().toString();
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            gmap.addMarker(new MarkerOptions().position(latLng).title(location));
+            gmap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            Toast.makeText(getApplicationContext(),address.getLatitude()+" "+address.getLongitude(),Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void onValide(View view) {
+
+    }
+
+    public void onNew(View view) {
+    }
+
+    public void onChangeType(View view) {
+    }
+
+    public void onTest(View view) {
     }
 }
